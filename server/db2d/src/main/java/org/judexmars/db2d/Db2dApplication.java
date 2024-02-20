7b71ae419e6b22464149d3067a468f4dbf2559ec
JudexMars <danielpro2013rus@gmail.com>
2024-02-20 23:16:31 +0300

    build: init back project & add essential dependencies

---
+++ server/db2d/src/main/java/org/judexmars/db2d/Db2dApplication.java
@@ -0,0 +1,13 @@
+package org.judexmars.db2d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2dApplication.class, args);
+    }
+
+}
