8f26f627126e92a4b3343a8706920d8d559c9e55
NikitaNpl <totmyanin.n@bk.ru>
2024-03-04 10:30:37 +0300

    CLIENT: added example of using styled-component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,56 @@
-import "./App.css";
+import styled from "styled-components";
 import logo from "./logo.svg";
+
+const StyledApp = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+
+  & > a {
+    color: #61dafb;
+  }
+`;
+
+const Img = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <StyledApp>
+      <Header>
+        <Img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
-    </div>
+      </Header>
+    </StyledApp>
   );
 }
 
